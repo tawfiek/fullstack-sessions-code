4264263f471ffd040bc11d9c4d86c574fa67539b
Tawfiek <>
2022-10-25 08:33:47 +0200

    add main middlewares

--- session-5/src/index.ts
+++ session-5/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import router from './routes';
 import * as dotenv from 'dotenv';
+import { errorHandler } from './middlewares/errorHandler';
+import logger from './middlewares/logger';
 
 dotenv.config();
 
@@ -10,7 +12,9 @@ const environment = process.env.NODE_ENV || 'development';
 const app = express();
 
 app.use(express.json());
+app.use(logger);
 app.use(router);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT} environment ${environment}`);
